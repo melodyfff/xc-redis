c8ab5d46a26c055dca9b92aeb068ed6d9f93f735
melodyfff <307208327@qq.com>
2018-12-04 15:10:45 +0800

    :smile: change 2 springboot2

---
+++ src/main/java/com/xinchen/redis/App.java
@@ -0,0 +1,18 @@
+package com.xinchen.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xinchen
+ * @version 1.0
+ * @date 04/12/2018 14:16
+ */
+@SpringBootApplication
+public class App {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/test/java/com/xinchen/redis/AppTest.java
@@ -0,0 +1,21 @@
+package com.xinchen.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author xinchen
+ * @version 1.0
+ * @date 04/12/2018 15:06
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+    @Test
+    public void initContext(){}
+}
